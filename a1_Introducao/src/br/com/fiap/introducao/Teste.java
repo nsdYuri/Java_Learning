bb77b6a49cc49b8590feb2700cd24e5a2c2976ce
Yuri <yurichichedom808@gmail,com>
2024-04-07 21:56:11 -0300

    Aprendendo sobre os tipos primitivos no java

--- a1_Introducao/src/br/com/fiap/introducao/Teste.java
+++ a1_Introducao/src/br/com/fiap/introducao/Teste.java
@@ -3,6 +3,27 @@ package br.com.fiap.introducao;
 public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Ola mundo!");
+		
+		
+		// Tipos primitivos:
+		
+		// Lógico - boolean
+		boolean estudando = true; // 8 bits
+		
+		// 1 caractere - char
+		// OBS: Entre aspas simples
+		char primeiraLetra = 'a'; // 16 bits
+		
+		// Numeros inteiros - int
+		byte idade = 19; // 8 bits
+		short altura = 185; // 16 bits
+		int anoNascimento = 2004; // 32 bits
+		long cep = 05516030; // 64 bits
+		
+		// Numeros reais ou Numeros com Ponto flutuante - double ou float
+		double peso = 81.24;
+		float salaraio = 5082.98f; // f no final é para inidcar que se trata de um float
+		
 	}
 
 }
